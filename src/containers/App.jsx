15f03a730817d2c3e09263a0c4fe585c12097568
Lesly <61633431+lesgom1990@users.noreply.github.com>
2020-06-17 20:39:21 -0500

    Carosuel, footer, header and nested structure  added

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import Header from '../components/Header';
+import '../components/Search'
 import '../assets/styles/App.scss';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
+import Search from '../components/Search';
+
 
 const App = () => (
     <div className="App">
         <Header />
+        <Search />
+        
+
+        <Categories>
+            <Carousel>
+                <CarouselItem/>
+                <CarouselItem/>
+                <CarouselItem/>
+                <CarouselItem/>
+                <CarouselItem/>
+                <CarouselItem/>
+                <CarouselItem/>
+                <CarouselItem/>
+            </Carousel>
+        </Categories>
+
+        <Footer />
     </div>
 );
 
